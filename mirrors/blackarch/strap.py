94f3a92df2fe7ae8c477243a4bf2ad9596fb4578
Hime <rodrigoalonsoaltamirano@gmail.com>
2026-07-08 20:48:16 -0400

    new changes

--- mirrors/blackarch/strap.py
+++ mirrors/blackarch/strap.py
@@ -213,14 +213,13 @@ def blackarch_setup():
     msg_print('You can install `blackarch-officials` metapackage with the most popular tools using the command below:')
     msg_print('sudo pacman -S --needed blackarch-officials')
 
-def nalca_install(root="/mnt"):
+def nalca_install(root="/mnt", user: str = ""):
     msg_print(f"Installing BlackArch mirrors into target {root} via arch-chroot...")
     script_path = os.path.abspath(__file__)
-    dest_path = os.path.join(root, "tmp", "strap.py")
-    os.makedirs(os.path.join(root, "tmp"), exist_ok=True)
+    dest_path = os.path.join(root, "home", user, "strap.py")
+    os.makedirs(os.path.join(root, "home", user), exist_ok=True)
     shutil.copy2(script_path, dest_path)
-    # subprocess.run(["arch-chroot", root, "chmod", "+x", "/tmp/strap.py"], check=True)
-    subprocess.run(["arch-chroot", root, "/tmp/strap.py"], check=True)
+    subprocess.run(["arch-chroot", root, f"/home/{user}/strap.py"], check=True)
     try:
         os.remove(dest_path)
     except OSError:
