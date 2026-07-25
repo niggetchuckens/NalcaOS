de4c0c584786cc8c83dc4aa029df08961f4528b2
Hime <rodrigoalonsoaltamirano@gmail.com>
2026-07-25 12:20:02 -0400

    FEAT: shell script working with try/except if homelab not working

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-mkdir -pv nalcaos && cd nalcaos
-
 URL="http://minio-api.hime-code.xyz/nalcaos"
-FILES=$(curl -sS $URL)
 
 echo "Downloading needed packages..."
-
-echo "$FILES" | grep -oP '(?<=<Key>).*?(?=</Key>)' | while read -r ITEM; do
-    echo "Downloading $ITEM..."
-    curl -sS --create-dirs "$URL/$ITEM" -o "$ITEM" 
-done
-
-python3 main.py
+if FILES=$(curl -sS $URL); then
+    mkdir -pv nalcaos && cd nalcaos
+    echo "$FILES" | grep -oP '(?<=<Key>).*?(?=</Key>)' | while read -r ITEM; do
+        echo "Downloading $ITEM..."
+        curl -sS --create-dirs "$URL/$ITEM" -o "$ITEM" 
+    done
+else
+    pacman -Sy --noconfirm git
+    git clone https://github.com/niggetchuckens/nalcaos
+    cd nalcaos 
+fi
+python3 main.py && rm -rf nalcaos && umount -R /mnt && reboot now
